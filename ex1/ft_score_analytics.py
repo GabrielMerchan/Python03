ae649305a01abfad0310a144c697af7574869c52
MikeWazowsky <gabriel.merchan.romero@gmail.com>
2026-04-29 20:28:21 +0200

    hasta 2 ej

---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+class Usage(Exception):
+    def __init__(self, err: str = "unknown error"):
+        super().__init__(err)
+
+
+def input_score(score_str: str) -> int:
+    num = int(score_str)
+    return num
+
+
+def check_scores(scores: list) -> list:
+    if len(scores) == 0:
+        raise Usage("python3 ft_score_analytics.py <score1> <score2> ...")
+    i = 1
+    score_good = []
+    cont = len(scores)
+    for i in scores:
+        try:
+            score_good.append(input_score(i))
+        except ValueError:
+            print(f"Invalid parameter: '{i}'")
+            cont -= 1
+    if cont == 0 or len(score_good) == 0:
+        raise Usage("python3 ft_score_analytics.py <score1> <score2> ...")
+    return score_good
+
+
+def add_scores() -> list:
+    score_list = sys.argv[1:]
+    scores = []
+    try:
+        scores = check_scores(score_list)
+    except Usage as e:
+        print(f"No scores provided. {e.__class__.__name__}: {e}")
+        sys.exit()
+    return scores
+
+
+def display_scores(scores: list) -> None:
+    print(f"Total players: {len(scores)}")
+    print(f"Total score: {sum(scores)}")
+    print(f"Average score: {sum(scores) / len(scores)}")
+    print(f"High score: {max(scores)}")
+    print(f"Low score: {min(scores)}")
+    print(f"Score range: {max(scores) - min(scores)}")
+
+
+if __name__ == "__main__":
+    print("=== Player Score Analytics ===")
+    scoring = add_scores()
+    display_scores(scoring)
